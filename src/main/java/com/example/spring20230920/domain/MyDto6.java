d74c86daf9822ca5aa630666ddc8fc92c667a6a4
Garbi93 <iseung2093@gmail.com>
2023-09-21 15:15:04 +0900

    9/21 property, lombok

---
+++ src/main/java/com/example/spring20230920/domain/MyDto6.java
@@ -0,0 +1,9 @@
+package com.example.spring20230920.domain;
+
+import lombok.Data;
+
+@Data   // 제일 많이 쓴다 -> 한번에 다 만들어준다 ㅋㅋㅋ
+public class MyDto6 {
+    private String name;
+    private Integer age;
+}

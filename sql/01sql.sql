b95608aae71ffbfb92ceeb8e1521fb072f6707d7
Garbi93 <iseung2093@gmail.com>
2023-10-04 10:55:18 +0900

    10/04 sql연습

---
+++ sql/01sql.sql
@@ -0,0 +1,11 @@
+-- 주석
+# 주석
+select * from w3schools.customers;
+select * from w3schools.employees;
+select *
+from w3schools.customers;
+
+-- 쿼리 작성 키워드는 대소문자 구분 안함
+SELECT * FROM w3schools.customers;
+
+-- 섞어쓰면 구분이 잘 안됨 보통 키워드는 대문자, 나머지는 소문자로 작성한다.(관습상)

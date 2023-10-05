e93922ccb5ca21db930940963245afc4fd1e92f5
Garbi93 <iseung2093@gmail.com>
2023-10-05 14:27:01 +0900

    10/05 sql and or

--- sql/06andor.sql
+++ sql/06andor.sql
@@ -39,6 +39,18 @@ WHERE CustomerName >= 'c' AND CustomerName < 'd';
 SELECT * FROM employees
 WHERE BirthDate >= '1958-01-01' AND BirthDate < '1959-01-01';
 
+-- 예) 스웨덴이나 이탈리아에 있는 공급자들 조회
+SELECT * FROM suppliers
+WHERE Country = 'sweden' OR Country = 'Italy';
+-- 예) 1963년 8월 생 직원 조회
+SELECT * FROM employees
+WHERE BirthDate >= '1963-08-01' AND BirthDate < '1963-09-01';
+-- 예) 가격이 100.00~200.00 (포함) 사이인 상품 조회
+SELECT * FROM products
+WHERE Price >= 100.00 AND Price <= 200.00;
+-- 예) 1997년에 주문한 주문 조회
+SELECT * FROM orders
+WHERE OrderDate >= '1997-01-01' AND OrderDate < '1998-01-01';
 
 
 

4c8098264e5f7248318441e38b4a5c9a1634ef04
Garbi93 <iseung2093@gmail.com>
2023-10-05 09:37:21 +0900

    10/05 view 포워드 , where활용하여 원하는 값만 보이게 하기

--- src/main/java/com/example/spring20230920/domain/MyDto16.java
+++ src/main/java/com/example/spring20230920/domain/MyDto16.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class MyDto16 {
-    private String pid;
+    private Integer pid;
     private String productName;
     private String unit;
     private String price;

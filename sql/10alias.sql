6b3e09aacbc738b844cbbe9e8e504968230d4feb
Garbi93 <iseung2093@gmail.com>
2023-10-06 10:17:15 +0900

    10/06 sql 별병별칭으로 변경하기(alias)

--- sql/10alias.sql
+++ sql/10alias.sql
@@ -12,3 +12,20 @@ SELECT
     EmployeeID,
     CONCAT(FirstName, ' ', LastName) AS fullname
 FROM employees;
+SELECT
+    EmployeeID,
+    CONCAT(FirstName, ' ', LastName) fullname
+FROM employees; -- AS를 안써도 별칭이 적용이 가능하다.
+SELECT
+    EmployeeID,
+    CONCAT(FirstName, ' ', LastName) `full name`
+FROM employees; -- 띄어쓰기나 키워드를 넣고싶으면 ``안에 넣어 사용 하면 된다
+
+-- 예) 공급자의 이름(suppliersName), 주소(address, city, country)연결
+-- 전화번호(phone -> phone number)
+-- supplierName, address, `phone number`
+SELECT
+    SupplierName,
+    CONCAT(Address, ' ',City, ' ', Country) address,
+    Phone `phone number`
+FROM suppliers;

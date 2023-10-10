2f3fcab8ba0b9130b995d9280dda138616791994
Garbi93 <iseung2093@gmail.com>
2023-10-10 16:47:40 +0900

    10/10 sql insert into 명령어 활용

--- src/main/java/com/example/spring20230920/controller/Controller24.java
+++ src/main/java/com/example/spring20230920/controller/Controller24.java
@@ -109,8 +109,8 @@ public class Controller24 {
         try(connection; statement) {
             statement.setString(1, employee.getLastName());
             statement.setString(2, employee.getFirstName());
-            statement.setString(3, employee.getBirthDate());
-            //statement.setTimestamp(3, Timestamp.valueOf(employee.getBirthDate().atStartofDay()));
+            //statement.setString(3, employee.getBirthDate());
+            statement.setTimestamp(3, Timestamp.valueOf(employee.getBirthDate().atStartOfDay()));
             statement.setString(4, employee.getPhoto());
             statement.setString(5, employee.getNotes());
             int count = statement.executeUpdate();

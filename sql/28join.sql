f1e9407bfa5c15a5d215f58beab3762a8735a544
Garbi93 <iseung2093@gmail.com>
2023-10-12 16:05:55 +0900

    10/12 sql join

--- sql/28join.sql
+++ sql/28join.sql
@@ -61,3 +61,59 @@ SELECT ProductID,ProductName,CategoryName
 FROM products AS p JOIN categories AS c
 ON p.CategoryID = c.CategoryID
 WHERE ProductName = 'chais';
+
+-- 예) supplier, products 테이블을 조인 하여 chais 상품의 공급자명, 전화번호 조회
+SELECT SupplierName, Phone
+FROM suppliers AS sup JOIN products AS pro
+ON sup.SupplierID = pro.SupplierID
+WHERE ProductName = 'chais';
+-- 예) 1996년 7월 4일 에 주문한 고객명 (customer, orders)
+SELECT CustomerName
+FROM customers JOIN orders
+ON customers.CustomerID = orders.CustomerID
+WHERE OrderDate = '1996-07-04';
+-- 예) 1996년 7월 4일 에 주문을 담당한 직원명 (employees, orders)
+SELECT LastName, FirstName
+FROM employees JOIN orders
+ON employees.EmployeeID = orders.EmployeeID
+WHERE OrderDate = '1996-07-04';
+
+SELECT COUNT(*) FROM products; -- 77
+SELECT COUNT(*) FROM suppliers; -- 29
+SELECT COUNT(*) FROM categories; -- 8
+SELECT 77 * 29 * 8;
+-- 3개의 테이블 조회
+SELECT * FROM products, suppliers, categories; -- 17864
+
+SELECT p.ProductName, s.SupplierName, s.Phone, c.CategoryName
+FROM products p
+    JOIN suppliers s
+        ON p.SupplierID = s.SupplierID
+    JOIN categories c
+        ON p.CategoryID = c.CategoryID
+WHERE ProductName = 'chais'; -- 여러 테이블 join, on 시키는 방법
+
+
+-- 예) 'chang'상품의 공급자명, 카테고리명을 조회
+SELECT p.ProductName, c.CategoryName
+FROM products p JOIN categories c
+ON p.CategoryID = c.CategoryID
+WHERE p.ProductName = 'chang';
+
+-- 예) '1996-07-09'에 주문한고객명과, 처리한 직원명
+SELECT c.CustomerName, e.LastName, e.FirstName
+FROM orders o JOIN customers c
+ON o.CustomerID = c.CustomerID
+JOIN employees e
+ON o.EmployeeID = e.EmployeeID
+WHERE o.OrderDate = '1996-07-09';
+
+-- 예) '1996-07-09'에 주문한 상품명 (orders, orderDetail, products)
+SELECT p.ProductName
+FROM orders o JOIN orderdetails od
+ON o.OrderID = od.OrderID
+JOIN products p
+ON od.ProductID = p.ProductID
+WHERE o.OrderDate = '1996-07-09';
+
+

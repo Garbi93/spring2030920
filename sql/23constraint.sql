513166bb19da4b965c847aa6b3f5acd43af89a3e
Garbi93 <iseung2093@gmail.com>
2023-10-12 09:43:25 +0900

    10/12 sql constraint

--- sql/23constraint.sql
+++ sql/23constraint.sql
@@ -20,3 +20,93 @@ INSERT INTO my_table13 (col1)
 VALUE (66); -- not ok
 
 SELECT * FROM my_table13;
+
+--
+-- UNIQUE : 해당 컬럼에 유일한값을 허용할지 안 할지
+CREATE TABLE my_table14 (
+    col1 INT,
+    col2 INT UNIQUE  -- 이 컬럼에 중복된 값을 넣을 수 없음
+);
+INSERT INTO my_table14 (col1, col2)
+VALUE (11,22); -- ok
+INSERT INTO my_table14 (col1, col2)
+VALUE (11,34); -- ok
+INSERT INTO my_table14 (col1, col2)
+VALUE (11, 34); -- not ok 이미 위에서 col2에 34 를 넣어서
+INSERT INTO my_table14 (col1)
+VALUE (11); -- 이때 col2에 는 null 이지만 null은 unique 이라도 중복 입력이 가능하다.
+
+SELECT * FROM my_table14;
+
+
+--
+-- 그렇다면 null 도 중복불가능 하게 하려면 not null을 쓰자
+CREATE TABLE my_table15 (
+    col1 INT,
+    col2 INT NOT NULL ,
+    col3 INT UNIQUE ,
+    col4 INT NOT NULL UNIQUE
+);
+INSERT INTO my_table15 (col1, col2, col3, col4)
+VALUE (11,22,33,44); -- ok
+INSERT INTO my_table15 (col1, col2, col3, col4)
+    VALUE (11,22,33,44); -- not ok col3, col4가 중복이 허용이 아니기 때문
+INSERT INTO my_table15 (col1, col2, col3, col4)
+VALUE (11,22,44,NULL); -- not ok col4가 null을 허용을 안 하기 때문에
+INSERT INTO my_table15 (col1, col2, col3, col4)
+    VALUE (11,22,44,55); -- ok
+
+-- 예) my_table16 지금까지 배운 제약 사항을 사용 해서 연습해보자
+CREATE TABLE my_table16(
+    colName VARCHAR(10) NOT NULL,
+    colAge INT ,
+    colPhone VARCHAR(15) NOT NULL UNIQUE ,
+    colAddress VARCHAR(30),
+    colNotes VARCHAR(100)
+);
+INSERT INTO my_table16 (colName, colAge, colPhone, colAddress, colNotes)
+VALUE ('홍길동',30,'010',null,null); -- ok
+INSERT INTO my_table16 (colName, colPhone)
+    VALUE ('홍길동','010'); -- not ok
+INSERT INTO my_table16 (colName, colPhone)
+    VALUE ('임꺽정','010-1'); -- ok
+INSERT INTO my_table16 (colName, colAge, colPhone, colAddress, colNotes)
+VALUE ('이강인' ,20 , '0000','123ㅂㅈㄷ','ㅁㄴㅇ'); --  ok
+SELECT * FROM my_table16;
+
+--
+-- DEFAULT : 기본값
+CREATE TABLE my_table17 (
+    col1 INT,
+    col2 INT DEFAULT 100, -- 값을 넣지 않으면 100
+    col3 VARCHAR(10) DEFAULT 'empty', -- 값을 넣지 않으면 'empty'라는 스트링을 넣는다
+    col4 DATETIME DEFAULT NOW() -- 값을 넣지 않으면 현재 일시로 넣는다.
+);
+INSERT INTO my_table17 (col1, col2, col3, col4)
+VALUE (22, 33, 'son','2023-11-11 12:30:21');
+INSERT INTO my_table17 (col1)
+    VALUE (222); -- col1 만 넣으면 나머지 들은 우리가 정해준 기본값이 들어간 것을 볼 수 있다.
+INSERT INTO my_table17 (col1, col2, col3, col4)
+    VALUE (22, null, null,null); -- 기본값이 설정 되어있을뿐 null을 사용 할 수도 있다.
+
+SELECT * FROM my_table17;
+
+CREATE TABLE my_table18 (
+    col1 INT,
+    col2 INT DEFAULT 300,
+    col3 INT NOT NULL DEFAULT 500
+);
+INSERT INTO my_table18 (col1, col2, col3)
+VALUE (3, NULL, NULL); -- not ok
+INSERT INTO my_table18 (col1, col3)
+    VALUE (3, 222); -- ok
+INSERT INTO my_table18 (col1)
+VALUE (4); -- ok
+SELECT * FROM my_table18;
+
+
+CREATE TABLE my_table19 (
+    col1 INT NOT NULL UNIQUE DEFAULT 100
+);
+
+

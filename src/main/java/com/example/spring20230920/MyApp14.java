e961a4f118c87fed71e5f4b4c2b81eb3623a7713
Garbi93 <iseung2093@gmail.com>
2023-10-17 15:10:50 +0900

    10/17 @Configuration 내부 Bean

---
+++ src/main/java/com/example/spring20230920/MyApp14.java
@@ -0,0 +1,45 @@
+package com.example.spring20230920;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+@SpringBootApplication
+public class MyApp14 {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(MyApp14.class, args);
+        Object myBean1 = context.getBean("myBean1");
+        System.out.println("myBean1 = " + myBean1);
+        Object myBean2 = context.getBean("myBean2");
+        System.out.println("myBean2 = " + myBean2);
+    }
+
+}
+
+// Configuration 클래스(빈) :
+// spring bean을 만드는 메소드를 가진 클래스
+
+@Configuration
+class Configuration1 {
+
+    // 빈을 만드는 메소드
+    // bean의 이름은 메소드명과 같음
+    @Bean
+    public MyClass27 myBean1 () {
+        return new MyClass27();
+    }
+
+
+    @Bean
+    public MyClass27 myBean2() {
+        return new MyClass27();
+    }
+}
+
+// @Component // 우리가 만든것은 붙여줄 수 있다.. // 빈의 이름이 클래스 이름을 lowerCamelCase로 바꾼 것
+class MyClass27 {
+
+}

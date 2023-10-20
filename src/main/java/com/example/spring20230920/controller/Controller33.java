bb54e3ab8179be460d1d30c5143de6d904f99c7d
Garbi93 <iseung2093@gmail.com>
2023-10-20 14:44:56 +0900

    10/20 file업로드 시키기

---
+++ src/main/java/com/example/spring20230920/controller/Controller33.java
@@ -0,0 +1,82 @@
+package com.example.spring20230920.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("main33")
+public class Controller33 {
+
+    @GetMapping("sub1")
+    public void method1() {
+
+    }
+
+    @PostMapping("sub2")
+    public void method2(String name, Integer age, MultipartFile myfile) throws IOException {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+
+        if(myfile != null ){
+            String fileName = myfile.getOriginalFilename();
+            System.out.println("fileName = " + fileName);
+
+
+            String path = "c:\\Temp\\upload_" + fileName;
+            InputStream inputStream = myfile.getInputStream();
+            FileOutputStream outputStream = new FileOutputStream(path);
+
+            BufferedInputStream bis = new BufferedInputStream(inputStream);
+            BufferedOutputStream bos = new BufferedOutputStream(outputStream);
+
+            try(bis; bos; inputStream; outputStream) {
+                byte[] data = new byte[1024];
+                int len = 0;
+
+                while ((len = bis.read(data)) != -1) {
+                    bos.write(data,0,len);
+                }
+                bos.flush();
+            }
+
+        }
+    }
+
+
+    @GetMapping("sub3")
+    public void method3() {
+
+    }
+
+    @PostMapping("sub4")
+    public void method4(MultipartFile upload) throws IOException {
+        // 업로드한 파일을 C:\\Temp\\ 폴더에 저장하기
+        if(upload != null) {
+            String fileName = upload.getOriginalFilename();
+            System.out.println("fileName = " + fileName);
+
+            String path = "C:\\Temp\\upload_"+fileName;
+            InputStream inputStream = upload.getInputStream();
+            FileOutputStream outputStream = new FileOutputStream(path);
+
+            BufferedInputStream bis = new BufferedInputStream(inputStream);
+            BufferedOutputStream bos = new BufferedOutputStream(outputStream);
+
+            try (inputStream; outputStream; bis; bos) {
+                byte[] data = new byte[1024];
+                int len = 0;
+                while ((len = bis.read(data)) != -1){
+                    bos.write(data, 0, len);
+                }
+            }
+            bos.flush();
+        }
+    }
+}

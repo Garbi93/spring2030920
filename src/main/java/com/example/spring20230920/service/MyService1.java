b8142a97c604c9569af10d7d8b607138551652c1
Garbi93 <iseung2093@gmail.com>
2023-10-23 09:10:07 +0900

    10/20 file업로드 시키기

--- src/main/java/com/example/spring20230920/service/MyService1.java
+++ src/main/java/com/example/spring20230920/service/MyService1.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(rollbackFor = Exception.class) // 하나의 업무처리로 실행 시키고 싶을때에
 public class MyService1 {
 
-
     private final MyDao6 dao;
 
     //@Transactional(rollbackFor = Exception.class) // 하나의 업무처리로 실행 시키고 싶을때에
@@ -21,6 +20,5 @@ public class MyService1 {
         dao.update1();
         int c = 1/0; //runtimeException -> 강제로 exeption 을 발생시켜서 rollback시키게 하기
         dao.update2();
-
     }
 }

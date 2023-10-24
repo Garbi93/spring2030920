634b3ab1e0537a0cafd230f16248cea78971d447
Garbi93 <iseung2093@gmail.com>
2023-10-24 14:25:50 +0900

    10/24 axios post요청 dto에 List타입과 Map타입

---
+++ src/main/java/com/example/spring20230920/controller/Controller37.java
@@ -0,0 +1,144 @@
+package com.example.spring20230920.controller;
+
+import com.example.spring20230920.domain.MyDto42;
+import com.example.spring20230920.domain.MyDto43;
+import com.example.spring20230920.domain.MyDto44;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("main37")
+@RequiredArgsConstructor
+public class Controller37 {
+
+    /*
+    axios.post("/main37/sub1",'{"name":"jones"}',{
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    ->json타입으로 전송 (직접 쓸 필요가 없다)
+
+    axios.post("/main37/sub1",{name:"jones"})
+     -> 객체만 작성해도 된다.
+     */
+
+    // 직렬화 (serialization)
+    // js(java) Object -> json text
+    // 파싱 parse
+    // json text -> js(java) Object
+    @PostMapping("sub1")
+    public void method1(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+    }
+
+
+    /*
+    axios.post("/main37/sub2", {name: "lee", age: 33, score:8.9})
+     */
+
+    /*
+    js Object
+    {name: "lee", age: 33, score:8.9}
+    ->(직렬화)
+    json text
+    {"name": "lee", "age": 33, "score":8.9}
+    ->(파싱)
+    java map
+    {name: "lee", age: 33, score:8.9}
+     */
+    @PostMapping("sub2")
+    public void method2(@RequestBody Map<String, Object> map) {
+        // @RequestBody 어노테이션을 보고 spring이 파싱해서 map에 넣는 다는것을 인식한다.
+        System.out.println("map = " + map);
+    }
+
+
+    /*
+    axios.put("/main37/sub3",{
+        city: "seoul",
+        name: "son",
+        email: "abe@gmail.com"
+    })
+     */
+    @PutMapping("sub3")
+    public void method3(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+    }
+
+    /*
+    axios.post("/main37/sub4", {
+            city:"paris",
+            age: 30,
+            married: true,
+            car: null,
+            home: {
+                address: "seoul",
+                price: 1000
+            },
+            foods: [
+                "pizza",
+                "buger",
+                "부침개"
+            ]
+            })
+     */
+    @PostMapping("sub4")
+    public void method4(@RequestBody MyDto42 dto) {
+        System.out.println("dto = " + dto);
+
+    }
+
+
+    //js로 ajax요청하는 코드 실행(axios)
+    /*
+    axios.post("/main37/sub5",{
+        country: "korea",
+        score: 30.2,
+        skills: [
+            "축구",
+            "야구",
+            "농구"
+        ],
+        phone: "010-1234"
+    })
+     */
+    @PostMapping("sub5")
+    public void method5(@RequestBody MyDto43 dto) {
+        System.out.println("dto.getCountry() = " + dto.getCountry()); // String
+        System.out.println("dto.getScore() = " + dto.getScore()); // Double
+        System.out.println("dto.getSkills() = " + dto.getSkills()); // List<String>
+        System.out.println("dto.getPhone() = " + dto.getPhone()); // String
+    }
+
+
+    /*
+    axios.post("/main37/sub6",{
+        name: "son",
+        email: ["son@gmail.com","hm@gmail.com"],
+        home: {
+            first: "seoul",
+            second: "london"
+        },
+        price: 35.11,
+        weight: 77.6,
+        married: true
+    })
+     */
+    @PostMapping("sub6")
+    public void method6(@RequestBody MyDto44 dto) {
+        System.out.println("dto.getName() = " + dto.getName());
+        System.out.println("dto.getEmail() = " + dto.getEmail());
+        System.out.println("dto.getHome() = " + dto.getHome());
+        System.out.println("dto.getPrice() = " + dto.getPrice());
+        System.out.println("dto.getWeight() = " + dto.getWeight());
+        System.out.println("dto.getMarried() = " + dto.getMarried());
+    }
+
+}

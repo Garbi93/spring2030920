0d2f26b0b480796744ef11e7cb54727930f127b3
Garbi93 <iseung2093@gmail.com>
2023-10-24 10:49:41 +0900

    10/24 axios delete 요청및 pathDelete 요청

--- src/main/java/com/example/spring20230920/controller/Controller36.java
+++ src/main/java/com/example/spring20230920/controller/Controller36.java
@@ -3,9 +3,9 @@ package com.example.spring20230920.controller;
 import com.example.spring20230920.dao.MyDao7;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Map;
 
 @Controller
@@ -29,12 +29,143 @@ public class Controller36 {
     }
 
     // /js 코드? : axios.get("/main36/sub3", {params: {id: 2}})
+    // axios.request({
+    //          url:"/main36/sub3",
+    //          method: "get",
+    //          params: {id: 2}
+    //          })
     // /main36/sub3?id=2
     @GetMapping("sub3")
     public void method3(Integer id) {
         Map<String, Object> data = dao.selectByEmployeeId(id);
         System.out.println("data = " + data);
     }
+
+
+    /*
+    axios.post("/main36/sub4",{
+        lastName: "손",
+        firstName: "흥민",
+        birthDate: "2020-02-02"
+    },{
+        headers: {
+            "content-type": "application/x-www-form-urlencoded"
+        }
+    })
+     */
+    // post /main36/sub4
+    // lastName=손&firstName=흥민&birthDate=2020-02-02
+    @PostMapping("sub4")
+    public void method4(String lastName,
+                        String firstName,
+                        LocalDate birthDate) {
+        int i = dao.insertEmployee(lastName, firstName, birthDate);
+        System.out.println(i+"개 데이터가 잘 입력되었습니다.");
+    }
+
+
+    // 브라우저 개발자 도구 콘솔에 axios 코드 작성 후 실행
+    // post /main36/sub5
+    // name=라면&price=150.00&category=3
+    /*
+    axios.post("/main36/sub5",{
+            name: "라면",
+            price: 150.00,
+            category: 3
+        },{
+        headers: {
+            "content-type": "application/x-www-form-urlencoded"
+        }
+    })
+     */
+    @PostMapping("sub5")
+    public void method5(String name,
+                        Double price,
+                        Integer category) {
+        int r = dao.insertProduct(name, price, category);
+        System.out.println(r+"개 행이 입력됨");
+    }
+
+
+
+
+    /*
+    axios.delete("/main36/sub6")
+     */
+    // @RequestMapping(method = RequestMethod.DELETE, value = "sub6")
+    @DeleteMapping("sub6") // 윗줄과 같은 일을함
+    public void method6() {
+        System.out.println("Controller36.method6");
+    }
+
+
+    /*
+    js 코드 작성후 브라우저 콘솔에 실행
+    axios.delete("/main36/sub7")
+     */
+    @DeleteMapping("sub7")
+    public void method7() {
+        System.out.println("Controller36.method7");
+    }
+
+    /*
+    axios.delete("/main36/sub8?name=son") // 이렇게도 가능하고, 아래로 도 가능하다.
+    axios.delete("/main36/sub8", {
+        params: {
+            name: "son"
+        }
+    })
+     */
+    @DeleteMapping("sub8")
+    public void method8(String name) {
+        System.out.println("name = " + name);
+    }
+
+
+    /*
+    axios.delete("/main36/sub9/son")
+    axios.delete("/main36/sub9/lee")
+     */
+    // PathVariable : 경로에 포함된 데이터
+    @DeleteMapping("sub9/{name}")
+    public void method9(@PathVariable("name") String n) { // pathvariable
+        System.out.println("n = " + n);
+    }
+
+    /*
+    axios.delete("/main36/sub10/3")
+    axios.delete("/main36/sub10/5")
+    axios.delete("/main36/sub10/7")
+     */
+    // delete /main36/sub10
+    @DeleteMapping("sub10/{number}")
+    public void method10(@PathVariable("number") Integer num) {
+        System.out.println("num = " + num);
+    }
+
+
+    /*
+    axios.delete("/main36/sub11/79")
+     */
+    @DeleteMapping("sub11/{pid}")
+    public void method11(@PathVariable Integer pid) {
+        int c = dao.deleteByProductId(pid);
+        System.out.println(c+"개 데이터 삭제됨");
+    }
+
+    /*
+    axios.delete("/main36/sub12/1")
+    axios.delete("/main36/sub12/3")
+    axios.delete("/main36/sub12/5")
+    // 고객 id 에 맞게 지워지도록 하기
+     */
+    @DeleteMapping("sub12/{cid}")
+    public void mehthod12(@PathVariable Integer cid) {
+        int cRow = dao.deleteByCustomerId(cid);
+        System.out.println(cRow + "만큼 고객이 지워졌습니다.");
+
+    }
+
 
 
 
--- src/main/java/com/example/spring20230920/dao/MyDao7.java
+++ src/main/java/com/example/spring20230920/dao/MyDao7.java
@@ -1,8 +1,11 @@
 package com.example.spring20230920.dao;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDate;
 import java.util.Map;
 
 @Mapper
@@ -19,4 +22,28 @@ public interface MyDao7 {
         WHERE EmployeeID = #{id}
         """)
     Map<String, Object> selectByEmployeeId(Integer id);
+
+    @Insert("""
+        INSERT INTO employees(LastName, FirstName, BirthDate)
+        VALUES (#{lastName},#{firstName},#{birthDate})
+        """)
+    int insertEmployee(String lastName, String firstName, LocalDate birthDate);
+
+    @Insert("""
+        INSERT INTO products(ProductName,Price, CategoryID)
+        VALUES (#{name},#{price},#{category})
+        """)
+    int insertProduct(String name, Double price, Integer category);
+
+    @Delete("""
+        DELETE FROM products
+        WHERE ProductID = #{pid}
+        """)
+    int deleteByProductId(Integer pid);
+
+    @Delete("""
+        DELETE FROM customers
+        WHERE CustomerID = #{cid}
+        """)
+    int deleteByCustomerId(Integer cid);
 }
